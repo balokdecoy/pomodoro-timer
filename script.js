9d0d49f3650d5c6e8cb93d474714769bd630a549
johnniesimpson <johnniesimpson@outlook.com>
2021-12-21 19:55:00 -0500

    Tinkering with timer.

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.getElementById("timer1").addEventListener("click", clickedButton);
 var clock = document.getElementById("clock");
 
-let pomodoro = 25;
+let pomodoro = 10;
 let time = setInterval(() => {
+    clock.innerHTML = pomodoro;
     console.log("tick", pomodoro--);
-    if (pomodoro === 0) {
+    if (pomodoro < 1) {
         clearInterval(time);
-        console.log("BOOM!!!")
+        clock.innerHTML = "BOOM!"
     }
 }, 2000)
 
 function clickedButton(e) {
   e.preventDefault();
   alert("you clicked me");
-  clock.innerHTML = 'pomodoro';
+  clock.innerHTML = pomodoro;
 }
