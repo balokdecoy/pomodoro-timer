2228071e09e43c2adcbd8d389e3dfe5cfff309d5
johnniesimpson <johnniesimpson@outlook.com>
2021-12-21 19:00:24 -0500

    First commit. Add basic file structure. Beginnings of functionality and content.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pomodoro-timer
 A pomodoro timer app
+
+This is just for fun and practice. A totally back to basics vanilla JavaScript pomodoro app. Spare time project. 
---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("timer1").addEventListener("click", clickedButton)
+
+function clickedButton(e) {
+    e.preventDefault()
+    alert("you clicked me")
+}
